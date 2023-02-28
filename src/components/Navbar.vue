e411b46f9d3c4e15a078db1bd8f41f1eae4e7333
nagao <itsonlf2525@gmail.com>
2023-03-01 00:24:47 +0900

    logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,18 +6,51 @@
         >さん
       </p>
       <p class="email">現在、 {{ email }} でログイン中です</p>
+      <div class="error">{{ error }}</div>
     </div>
-    <button>ログアウト</button>
+    <button @click="logout">ログアウト</button>
   </nav>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       name: window.localStorage.getItem("name"),
       email: window.localStorage.getItem("uid"),
+      error: null,
     };
+  },
+  methods: {
+    async logout() {
+      this.error = null;
+      try {
+        const res = await axios.delete("http://localhost:3000/auth/sign_out", {
+          headers: {
+            uid: this.email,
+            "access-token": window.localStorage.getItem("access-token"),
+            client: window.localStorage.getItem("client"),
+          },
+        });
+        if (!res) {
+          new Error("ログアウトできませんでした");
+        }
+
+        console.log("ログアウトしました");
+        window.localStorage.removeItem("access-token");
+        window.localStorage.removeItem("client");
+        window.localStorage.removeItem("uid");
+        window.localStorage.removeItem("name");
+        this.$router.push({ name: "Welcome" });
+
+        return res;
+      } catch (error) {
+        this.error = "ログアウトできませんでした";
+        console.log({ error });
+      }
+    },
   },
 };
 </script>
