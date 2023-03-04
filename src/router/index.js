b3f6daf34ab3b865f0114ccf35773c9056efad6e
nagao <itsonlf2525@gmail.com>
2023-03-04 14:29:53 +0900

    add navigation block

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,62 @@ import { createRouter, createWebHistory } from "vue-router";
 import Welcome from "../views/Welcome.vue";
 import ChatRoom from "../views/ChatRoom.vue";
 
+import useValidate from "../auth/validate";
+
+const { validate, error } = useValidate();
+
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem("uid");
+  const client = window.localStorage.getItem("client");
+  const accessToken = window.localStorage.getItem("access-token");
+
+  if (!uid || !client || !accessToken) {
+    console.log("ログインしていません");
+    next({ name: "Welcome" });
+    return;
+  }
+
+  await validate();
+
+  if (error.value) {
+    console.log("認証に失敗しました");
+    next({ name: "Welcome" });
+  } else {
+    next();
+  }
+};
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem("uid");
+  const client = window.localStorage.getItem("client");
+  const accessToken = window.localStorage.getItem("access-token");
+
+  if (!uid && !client && !accessToken) {
+    next();
+    return;
+  }
+
+  await validate();
+
+  if (!error.value) {
+    next({ name: "Chatroom" });
+  } else {
+    next();
+  }
+};
+
 const routes = [
   {
     path: "/",
     name: "Welcome",
     component: Welcome,
+    beforeEnter: noRequireAuth,
   },
   {
     path: "/chatroom",
     name: "ChatRoom",
     component: ChatRoom,
+    beforeEnter: requireAuth,
   },
 ];
 
