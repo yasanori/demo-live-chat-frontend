6a410303df18e88ce7baf3b3c4caaee46505fb97
nagao <itsonlf2525@gmail.com>
2023-03-08 23:32:51 +0900

    add message

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="chat-window">{{ messages }}</div>
+</template>
+
+<script>
+export default {
+  props: ["messages"],
+};
+</script>
+
+<style scoped>
+.chat-window {
+  background: white;
+  padding: 30px 20px;
+  border-bottom: 1px solid #eee;
+}
+</style>
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,14 +1,42 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Navbar from "../components/Navbar.vue";
+import ChatWindow from "../components/ChatWindow.vue";
 
 export default {
-  components: { Navbar },
+  components: { Navbar, ChatWindow },
+  data() {
+    return { messages: [] };
+  },
+  methods: {
+    async getMessage() {
+      try {
+        const res = axios.get("http://localhost:3000/messages", {
+          headers: {
+            uid: window.localStorage.getItem("uid"),
+            "access-token": window.localStorage.getItem("access-token"),
+            client: window.localStorage.getItem("client"),
+          },
+        });
+        if (!res) {
+          new Error("メッセージ一覧を取得できませんでした");
+        }
+        this.messages = res.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.getMessage();
+  },
 };
 </script>
 
